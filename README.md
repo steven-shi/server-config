4c4ec2074711117271dbf0f5d17382ddca88fbc6
Xiwen Shi <xshi@quantcast.com>
2025-02-06 21:57:52 +0000

    [main] setup script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # server-config
+
+```
+# server-config
+
+sh -c "$(curl -fsSL -H 'Authorization: token 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' https://raw.githubusercontent.com/steven-shi/server-config/main/setup.sh)"
+```
---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# zsh
+sudo apt update && sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+curl -fsSL https://raw.githubusercontent.com/steven-shi/server-config/main/.zshrc -o ~/.zshrc
+
+# Zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/mfaerevaag/wd.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/wd
+
+
+# ssh key
+mkdir -p ~/.ssh
+curl -fsSL https://raw.githubusercontent.com/username/repo/branch/id_rsa.pub -o ~/.ssh/authorized_keys
+
+# nvim setup
+sudo apt update
+sudo apt install neovim -y
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cat << EOF > ~/.config/nvim/init.vim
+if empty(glob('~/.local/share/nvim/site/autoload/plug.vim'))
+  silent !curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs
+      \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
+call plug#begin('~/.local/share/nvim/plugged')
+
+Plug 'elzr/vim-json'
+Plug 'dense-analysis/ale'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'prettier/vim-prettier', { 'do': 'yarn install' }
+
+call plug#end()
+EOF
+
+nvim +PlugInstall +qall
+
+chsh -s $(which zsh)
